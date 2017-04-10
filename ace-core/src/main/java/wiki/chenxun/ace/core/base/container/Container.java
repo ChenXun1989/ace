73c572dc87b0e65e11dbcb1dd8045225e5649faa
chenxun <xun.chen@ele.me>
2017-04-10 23:32:48 +0800

    spi改造，spring ioc 剥离

--- ace-core/src/main/java/wiki/chenxun/ace/core/base/container/Container.java
+++ ace-core/src/main/java/wiki/chenxun/ace/core/base/container/Container.java
@@ -3,39 +3,58 @@ package wiki.chenxun.ace.core.base.container;
 
 import wiki.chenxun.ace.core.base.annotations.Spi;
 
-import java.util.Observable;
 
 /**
  * @Description: Created by chenxun on 2017/4/10.
  */
 @Spi("spring")
-public interface Container   {
+public interface Container {
 
     /**
      * aceServicePackage
      */
     String ACE_SERVICE_PACKAGE = "aceServicePackage:";
 
-    void init( String[] args);
+    /**
+     * 初始化容器
+     *
+     * @param args 启动参数透传
+     */
+    void init(String[] args);
 
+    /**
+     * 容器开始
+     */
     void start();
 
+    /**
+     * 注册清理钩子
+     */
     void registerShutdownHook();
 
+    /**
+     * 停止容器
+     */
     void stop();
 
+    /**
+     * 获取实例
+     *
+     * @param t   t
+     * @param <T> 范型
+     * @return 实例
+     */
     <T> T getBean(Class<T> t);
 
-    <T> T getBean(Class<T> t,String name);
-
-
-
-
-
-
-
-
-
+    /**
+     * 获取实例
+     *
+     * @param t    class
+     * @param name name
+     * @param <T>  范型
+     * @return 实例
+     */
+    <T> T getBean(Class<T> t, String name);
 
 
 }
