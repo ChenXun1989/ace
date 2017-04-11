e265b518c65cb3862504c2ea658893200ff1b9a3
miaohao <8166405@qq.com>
2017-04-11 20:11:00 +0800

    消除AceService依赖Component

--- ace-core/src/main/java/wiki/chenxun/ace/core/base/annotations/AceService.java
+++ ace-core/src/main/java/wiki/chenxun/ace/core/base/annotations/AceService.java
@@ -17,7 +17,6 @@ import java.lang.annotation.Target;
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Component
 public @interface AceService {
     String value() default "";
     String path();
