d3aff9697f1e6203cd21a608c49d1adc3c5d208a
miaohao <8166405@qq.com>
2017-04-17 00:27:27 +0800

    GET POST PUT DELETE兼容

--- ace-core/src/main/java/wiki/chenxun/ace/core/base/remote/netty/DefaultDispatcher.java
+++ ace-core/src/main/java/wiki/chenxun/ace/core/base/remote/netty/DefaultDispatcher.java
@@ -3,7 +3,6 @@ package wiki.chenxun.ace.core.base.remote.netty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.netty.handler.codec.http.FullHttpRequest;
-import io.netty.handler.codec.http.HttpMethod;
 import io.netty.handler.codec.http.QueryStringDecoder;
 import io.netty.util.CharsetUtil;
 import wiki.chenxun.ace.core.base.annotations.AceHttpMethod;
@@ -15,6 +14,7 @@ import wiki.chenxun.ace.core.base.support.MethodParameter;
 
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
 
@@ -25,15 +25,6 @@ import java.util.Map;
  */
 public class DefaultDispatcher implements Dispatcher {
 
-    private Object parseObject(List<String> requestParamter, Class<?> paramterType) {
-        if (String.class.isAssignableFrom(paramterType)) {
-            return requestParamter.get(0);
-        } else if (Integer.class.isAssignableFrom(paramterType) || int.class.isAssignableFrom(paramterType)) {
-            return Integer.parseInt(requestParamter.get(0));
-        } else {
-            return null;
-        }
-    }
 
     /**
      * 请求分发与处理
@@ -52,34 +43,26 @@ public class DefaultDispatcher implements Dispatcher {
             return "";
         }
         Object aceServiceBean = Context.getBean(uri);
-        MethodDefine method = null;
-        if (request.method().equals(HttpMethod.GET)) {
-            QueryStringDecoder decoder = new QueryStringDecoder(request.uri());
-            method = Context.getMethodDefine(uri, AceHttpMethod.GET);
-            args = new Object[method.getParameters().size()];
-            Map<String, List<String>> map = decoder.parameters();
-            for (MethodParameter methodParameter : method.getParameters()) {
-                String parameterName = methodParameter.getParameterName();
-                Object paramterValue = parseObject(map.get(parameterName), methodParameter.getParameterType());
-                System.out.println("request:\t" + parameterName + "=" + paramterValue);
-                args[methodParameter.getParameterIndex()] = paramterValue;
-            }
+        AceHttpMethod aceHttpMethod = AceHttpMethod.getAceHttpMethod(request.method().toString());
 
-            System.out.println(decoder.parameters());
-            return method.getMethod().invoke(aceServiceBean, args);
-        } else if (request.method().equals(HttpMethod.POST)) {
+        MethodDefine method = Context.getMethodDefine(uri, aceHttpMethod);
+
+        String contentType = request.headers().get("Content-Type");
+
+        QueryStringDecoder decoder = new QueryStringDecoder(request.uri());
+
+        if (aceHttpMethod.equals(AceHttpMethod.GET) || aceHttpMethod.equals(AceHttpMethod.DELETE)) {
+            return this.excuteByParamters(aceServiceBean, method, decoder.parameters());
+        } else if (aceHttpMethod.equals(AceHttpMethod.POST) || aceHttpMethod.equals(AceHttpMethod.PUT)) {
             //如果要多次解析，请用 request.content().copy()
             String jsonStr = request.content().toString(CharsetUtil.UTF_8);
-            ObjectMapper om = new ObjectMapper();
+            ObjectMapper objectMapper = new ObjectMapper();
             try {
-                JsonNode node = om.reader().readTree(jsonStr);
-
-                System.out.println(node);
+                JsonNode node = objectMapper.reader().readTree(jsonStr);
+                return this.excuteByJson(aceServiceBean, method, objectMapper, node);
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
         }
         // TODO:路由方法
 
@@ -93,5 +76,65 @@ public class DefaultDispatcher implements Dispatcher {
         return mock;
     }
 
+
+    private Object excuteByJson(Object aceServiceBean, MethodDefine methodDefine, ObjectMapper objectMapper, JsonNode node) throws IOException, InvocationTargetException, IllegalAccessException {
+        Object[] args = new Object[methodDefine.getParameters().size()];
+        for (MethodParameter methodParameter : methodDefine.getParameters()) {
+            args[methodParameter.getParameterIndex()] = objectMapper.readValue(node.get(methodParameter.getParameterName()).traverse(), methodParameter.getParameterType());
+        }
+        return methodDefine.getMethod().invoke(aceServiceBean, args);
+    }
+
+    /**
+     * get 或post表单提交执行逻辑
+     *
+     * @param aceServiceBean
+     * @param method
+     * @param paramterMap
+     * @return
+     * @throws InvocationTargetException
+     * @throws IllegalAccessException
+     */
+    private Object excuteByParamters(Object aceServiceBean, MethodDefine method, Map<String, List<String>> paramterMap) throws InvocationTargetException, IllegalAccessException {
+        Object[] args = new Object[method.getParameters().size()];
+        for (MethodParameter methodParameter : method.getParameters()) {
+            String parameterName = methodParameter.getParameterName();
+            Object paramterValue = parseObject(paramterMap.get(parameterName), methodParameter.getParameterType());
+
+            System.out.println("request:\t" + parameterName + "=" + paramterValue);
+
+            args[methodParameter.getParameterIndex()] = paramterValue;
+        }
+        return method.getMethod().invoke(aceServiceBean, args);
+    }
+
+
+    private Object parseObject(List<String> requestParamter, Class<?> paramterType) {
+        if (String.class.isAssignableFrom(paramterType)) {
+            return getParamterStr(requestParamter);
+        } else if (Integer.class.isAssignableFrom(paramterType) || int.class.isAssignableFrom(paramterType)) {
+            return Integer.parseInt(getParamterStr(requestParamter));
+        } else if (Long.class.isAssignableFrom(paramterType) || long.class.isAssignableFrom(paramterType)) {
+            return Long.parseLong(getParamterStr(requestParamter));
+        } else if (Boolean.class.isAssignableFrom(paramterType) || boolean.class.isAssignableFrom(paramterType)) {
+            return Boolean.parseBoolean(getParamterStr(requestParamter));
+        } else if (Double.class.isAssignableFrom(paramterType) || double.class.isAssignableFrom(paramterType)) {
+            return Double.parseDouble(getParamterStr(requestParamter));
+        } else if (Float.class.isAssignableFrom(paramterType) || float.class.isAssignableFrom(paramterType)) {
+            return Float.parseFloat(getParamterStr(requestParamter));
+        } else if (Short.class.isAssignableFrom(paramterType) || short.class.isAssignableFrom(paramterType)) {
+            return Short.parseShort(getParamterStr(requestParamter));
+        }
+        return getParamterStr(requestParamter);
+    }
+
+    /**
+     * 获取请求参数 value
+     * @param requestParamter 请求参数聚合list
+     * @return value
+     */
+    private String getParamterStr(List<String> requestParamter) {
+        return requestParamter.get(0);
+    }
 
 }
